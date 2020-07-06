34a4f3eaa9f06ad867e69d05f56395c9ea4356c6
johnskimca <59780084+johnskimca@users.noreply.github.com>
2020-07-05 19:45:23 -0700

    Update todos.js

--- ToDoList/assets/js/todos.js
+++ ToDoList/assets/js/todos.js
@@ -1,20 +1,3 @@
-// Check off specific Todos by Clicking
-// Note the commented out code is a problem because
-// The dynamically rendered li's are not getting this toggle class
-
-// $('li').click(function (){
-//     $(this).toggleClass("completed")
-// })
-
-// ul must be selected because in jQuery
-// listeners can only be added to elements that exist
-// when the ul code below is run the 1st time
-// this means simply the dynamic rendered li's
-// cannot have listeners added to them
-
-// the selector li states that when an li inside of ul is clicked
-// run this code
-
 $('ul').on('click','li',function (){
     $(this).toggleClass("completed")
 })
